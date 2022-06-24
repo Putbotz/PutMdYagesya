d2a187763af85d1ab3d31b64c901addd2828a572
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-06-24 21:35:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ global.loadDatabase = async function loadDatabase() {
 }
 loadDatabase()
 
-global.authFile = `${opts._[0] || 'sus'}.data.json`
+global.authFile = `${opts._[0] || 'ika'}.data.json`
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
 const connectionOptions = {
